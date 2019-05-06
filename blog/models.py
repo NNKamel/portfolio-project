1a215e65a6c36fa722b51b658247051d58e8d742
NNKamel <noureldinashraf6@gmail.com>
2019-05-06 17:33:31 +0300

    home page linked to jobs and bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    # title, pub_date, body, image
+    title = models.CharField(max_length=20)
+    pub_date = models.DateTimeField(auto_now=True) #
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
