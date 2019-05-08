1123615da1ee1f0c19543bce7c699d797f615aba
NN <noureldinashraf6@gmail.com>
2019-05-08 14:26:32 +0300

    methods to tweak info of blog, blog html pages,

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,20 @@ from django.db import models
 class Blog(models.Model):
     # title, pub_date, body, image
     title = models.CharField(max_length=20)
-    pub_date = models.DateTimeField(auto_now=True) #
+    pub_date = models.DateTimeField() #
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        d = ["..."]
+        if(len(self.body)>100):
+            return self.body[:100]+ "..."
+        else:
+            return self.body
+
+    def out_date(self):
+        return self.pub_date.strftime('%d %B, %Y')
--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Job(models.Model): # models.Model links the class to the database
     image = models.ImageField(upload_to='images/')
     image_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.image_description
